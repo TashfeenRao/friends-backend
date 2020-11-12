bde949afc724f1b26c1ca2d094826eab3fc2f582
tashfeenrao <raotashfeen25@gmail.com>
2020-11-13 03:42:33 +0500

    added backend api

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '29edbb5a95efbe4326c820bceaff4334'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '303dd8b0-7857-4c15-8e23-0ef0705b3223'
+};
